85d96d161d8d975b0b4061412feef790dfee9b11
chris maley <chris.maley@speebly.com>
2020-01-27 17:27:52 -0500

    added ne version

---
+++ SpeeblyClientSDK.framework/Headers/SpeeblyClientSDK.h
@@ -0,0 +1,19 @@
+//
+//  SpeeblyClientSDK.h
+//  SpeeblyClientSDK
+//
+//  Created by Chris Maley on 2020-01-06.
+//  Copyright © 2020 Chris Maley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpeeblyClientSDK.
+FOUNDATION_EXPORT double SpeeblyClientSDKVersionNumber;
+
+//! Project version string for SpeeblyClientSDK.
+FOUNDATION_EXPORT const unsigned char SpeeblyClientSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpeeblyClientSDK/PublicHeader.h>
+
+
